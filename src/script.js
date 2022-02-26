0bb7d9f17fb217f70637fe13242eab3cafcfafe3
melibubbles <99666884+melibubbles@users.noreply.github.com>
2022-02-26 15:47:40 -0500

    Added the API call for the forecast
    Created function getForecast

--- src/script.js
+++ src/script.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function showForecast() {
+function showForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `
@@ -62,7 +63,14 @@ function showForecast() {
         </div>
     `;
   forecastElement.innerHTML = forecastHTML;
-  console.log(forecastHTML);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9b38a1b5ab26f73e5bf09125bd340224";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showWeather(response) {
@@ -87,6 +95,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function convertToFahrenheit(event) {
@@ -130,8 +140,6 @@ function getCurrentLocation(event) {
 }
 
 let celsiusTemperature = null;
-
-showForecast();
 
 let cityForm = document.querySelector("#search-form");
 cityForm.addEventListener("submit", handleSubmit);
